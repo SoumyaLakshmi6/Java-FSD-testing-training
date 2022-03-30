87f72579bacaa2295065cf498f239cc3ebf76749
SoumyaLakshmi6 <101250575+SoumyaLakshmi6@users.noreply.github.com>
2022-03-30 19:29:00 +0530

    Add files via upload

---
+++ Day5/TestAbstraction1.java
@@ -0,0 +1,37 @@
+package Day5;
+
+public class TestAbstraction1 {
+	abstract class Bike
+	{
+		Bike()//constructor
+		{
+			System.out.println("Bike class is invoked");
+		}
+		void run()//non- abstract method
+		{
+		System.out.println("bike is running");
+		}
+		abstract void display();//abstract method
+	}
+	class Suzuki extends Bike
+	{
+		void run()
+		{
+		System.out.println("Suzuki is running");
+		}
+		void display()// the abstract method is being implemented
+		{
+			System.out.println("Suzuki is a top tier bike");
+		} 
+	}
+	class TestAbstraction
+	{
+		public static void main(String[] args)
+		{
+			//Bike obj=new Suzuki();
+			//obj.run();
+			//obj.display();
+		}
+	}
+
+}
