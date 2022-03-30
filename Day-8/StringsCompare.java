a58274c04d1519963bb361ded28155f9ee5ba66b
SoumyaLakshmi6 <101250575+SoumyaLakshmi6@users.noreply.github.com>
2022-03-30 20:10:59 +0530

    Add files via upload

---
+++ Day-8/StringsCompare.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class StringsCompare
+{
+	public static void main(String[] args)
+	{
+		String s1="Amazon";
+		String s2="amazon";
+		String s4="Amazon";
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equalsIgnoreCase(s2));
+		String s3=new String("Amazon");
+		System.out.println(s1==s3);
+		System.out.println(s1==s4);
+
+	}
+}
